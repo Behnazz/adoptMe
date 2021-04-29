08ae6dece929e7c2d178db20f63f49b3cc4fe49c
Behnaz Pourafshari <behnaz.pourafshari@FT-MW5200.broadband>
2021-04-29 15:02:46 +0100

    WIP

---
+++ src/SearchParams.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropDown from "./useDropDown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropDown] = useDropDown("Animal", "", ANIMALS);
+  const [breed, BreedDropDown, setBreed] = useDropDown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  });
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropDown />
+        <BreedDropDown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
